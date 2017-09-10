cacae621638d6ffa5ed10e11e35caf46a446d57c
Jamal Natour <Jamal.Natour@fumasoftware.com>
2017-09-10 02:10:58 +0100

    Patch: add centos wrapper

---
+++ centos.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# workout the absolute path to the checkout directory
+abspath()
+{
+    case "${1}" in
+        [./]*)
+            local ABSPATH="$(cd ${1%/*}; pwd)/${1##*/}"
+            echo "${ABSPATH}"
+            ;;
+        *)
+            echo "${PWD}/${1}"
+            ;;
+    esac
+}
+
+SCRIPT=$(abspath ${0})
+SCRIPTPATH=`dirname ${SCRIPT}`
+ROOTPATH=`dirname ${SCRIPTPATH}`
+export PROJECT_ROOT=${ROOTPATH}
+
+scl enable devtoolset-1.1 ${PROJECT_ROOT}/scripts/run.sh
