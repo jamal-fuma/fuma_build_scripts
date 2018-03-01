71eabe2e61d1329d5257fad3b9774419550800d1
Jamal Natour <Jamal.Natour@fumasoftware.com>
2018-03-01 10:46:30 +0000

    Patch: add submodule-clone to git

---
+++ submodule-clone.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+# Reads a gitsubmodule file and clones the repos
+awk -F '=' '
+
+function to_clone_cmd(u,p){
+    printf("git clone %s ./%s\n",u,p);
+};
+
+/path/{
+    gsub(/\s+/,"",$2); path=$2;
+};
+/url/{
+    gsub(/\s+/,"",$2); url=$2;
+};
+
+/submodule/{
+    seen+=1;
+    if(seen > 1){
+        to_clone_cmd(url,path);
+    }
+    url="";
+    path="";
+};
+
+END{
+    to_clone_cmd(url,path);
+}' $1 | while read line; do
+    sh -xec "echo $line";
+done;
+
